c2579b07d8273f3067260fad4af22eff73000e9e
Holger Wingold <holger.wingold@adwizor.de>
2018-12-02 15:45:30 +0100

    Week2 Unit 2

--- application/src/main/java/com/sap/cloud/s4hana/examples/addressmgr/commands/GetAllBusinessPartnersCommand.java
+++ application/src/main/java/com/sap/cloud/s4hana/examples/addressmgr/commands/GetAllBusinessPartnersCommand.java
@@ -1,26 +1,57 @@
 package com.sap.cloud.s4hana.examples.addressmgr.commands;
 
-import com.sap.cloud.sdk.s4hana.datamodel.odata.helper.Order;
-import org.slf4j.Logger;
-
-import java.util.List;
-
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.sap.cloud.sdk.cloudplatform.cache.CacheKey;
 import com.sap.cloud.sdk.cloudplatform.logging.CloudLoggerFactory;
+import com.sap.cloud.sdk.s4hana.connectivity.CachingErpCommand;
+import com.sap.cloud.sdk.s4hana.datamodel.odata.helper.Order;
 import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.BusinessPartner;
 import com.sap.cloud.sdk.s4hana.datamodel.odata.services.BusinessPartnerService;
+import org.slf4j.Logger;
 
-public class GetAllBusinessPartnersCommand {
+import javax.annotation.Nonnull;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class GetAllBusinessPartnersCommand extends CachingErpCommand<List<BusinessPartner>> {
     private static final Logger logger = CloudLoggerFactory.getLogger(GetAllBusinessPartnersCommand.class);
 
     private static final String CATEGORY_PERSON = "1";
 
+    private static final Cache<CacheKey, List<BusinessPartner>> cache =
+            CacheBuilder.newBuilder()
+                    .maximumSize(50)
+                    .expireAfterWrite(5, TimeUnit.MINUTES)
+                    .build();
+
     private final BusinessPartnerService service;
 
     public GetAllBusinessPartnersCommand(final BusinessPartnerService service) {
+        super(GetAllBusinessPartnersCommand.class);
         this.service = service;
     }
 
-    public List<BusinessPartner> execute() throws Exception {
+    @Nonnull
+    @Override
+    protected Cache<CacheKey, List<BusinessPartner>> getCache() {
+        return cache;
+    }
+
+    @Override
+    protected CacheKey getCommandCacheKey() {
+        return CacheKey.ofTenantIsolation();
+    }
+
+    @Override
+    protected List<BusinessPartner> getFallback() {
+       logger.warn("Fallback called because of exception:", getExecutionException());
+       return Collections.emptyList();
+    }
+
+    @Override
+    protected List<BusinessPartner> runCacheable() throws Exception {
         return service.getAllBusinessPartner()
                 .select(BusinessPartner.BUSINESS_PARTNER, BusinessPartner.LAST_NAME, BusinessPartner.FIRST_NAME)
                 .filter(BusinessPartner.BUSINESS_PARTNER_CATEGORY.eq(CATEGORY_PERSON))
